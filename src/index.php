ff41eb64294f18360196d20362ef1f0ffe171030
DarlynYami05 <dwverling@gmail.com>
2021-08-28 18:57:29 -0500

    Permisos para consumo de web

--- src/index.php
+++ src/index.php
@@ -1,5 +1,7 @@
 <?php
-header("HTTP/1.1 200");
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers : Content-Type");
+header("Access-Control-Allow-Methods : POST, OPTIONS");
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     include('./../settings/post.php');
 }
